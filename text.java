1111758c6bbbee243bcc99c435b421d6588848b9
Girish GR <grgirish33@gmail.com>
2024-04-17 20:00:34 +0530

    Changes done-New Var

--- text.java
+++ text.java
@@ -2,6 +2,7 @@ class Test{
     public static void main(String[] args) {
         int a=10;
         int b=20;
-        System.out.println(a+b);
+        int c= 30;
+        System.out.println(a+b+c);
     }
 }
