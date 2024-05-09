3d298ec3fa83801028d30eb8708b8be786044783
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2024-05-09 21:01:16 +0530

    TsT

---
+++ JS - Part 1/one.js
@@ -0,0 +1,15 @@
+//create variable 
+
+var eid=101;
+
+//read variable
+console.log(eid)
+
+//update variable
+eid=201;
+console.log(eid)
+
+//delete variable
+//delete eid 
+eid=null
+console.log(eid)
---
+++ JS - Part 1/two.js
@@ -0,0 +1,16 @@
+var tv9="Hello";
+
+console.log(tv9);
+/*
+class Empl@yee{
+}
+*/
+class Employe{}
+
+let for1={}
+
+let extends="Priyanka"
+
+var a=100
+var A=200
+console.log(A)
