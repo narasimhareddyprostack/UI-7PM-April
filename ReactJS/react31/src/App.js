b6d34703970f7d9e56119322f2fb7ea8c8b7596c
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2024-06-18 21:02:19 +0530

    okie

---
+++ ReactJS/react31/src/App.js
@@ -0,0 +1,12 @@
+import Message from "./Message"
+let App = ()=>{
+
+    return <div>
+            <h2>Welcome to React world</h2>
+            <hr />
+            <Message/>
+          
+            </div>
+}
+
+export default App
---
+++ ReactJS/react31/src/index-1.js
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom'
+import App from './App'
+//ReactDOM.render("Hello,GE",document.getElementById('rajni'))
+//ReactDOM.render(<h1>Hello,GE</h1>,document.getElementById('rajni'))
+//ReactDOM.render(<h1>Hello,GE</h1><h2>Welcome to React</h2>,document.getElementById('rajni'))
+//ReactDOM.render(<div><h1>Hello,GE</h1><h2>Welcome to React</h2></div>,document.getElementById('rajni'))
+
+ReactDOM.render(<App/>,document.getElementById('rajni'))
